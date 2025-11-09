e7d2156015f184a02ad996be5bd88b69ad0536c6
Andi Zahiri <zahiriandii@gmail.com>
2025-11-09 23:07:53 +0100

    Project setup for BusApp - BackEnd

---
+++ BackEnd-BusApp/BusApp/src/main/java/com/andi/busapp/BusAppApplication.java
@@ -0,0 +1,13 @@
+package com.andi.busapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusAppApplication.class, args);
+    }
+
+}
---
+++ BackEnd-BusApp/BusApp/src/test/java/com/andi/busapp/BusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andi.busapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
