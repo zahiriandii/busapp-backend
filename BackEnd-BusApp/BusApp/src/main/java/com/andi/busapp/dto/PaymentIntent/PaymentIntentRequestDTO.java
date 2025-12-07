76366f2f81f8c07dd97ad184d7d81f3f7fbc2b7b
Andi Zahiri <zahiriandii@gmail.com>
2025-12-07 17:48:26 +0100

    Created the StripeConfig class and PaymentIntent request and response DTO's

---
+++ BackEnd-BusApp/BusApp/src/main/java/com/andi/busapp/dto/PaymentIntent/PaymentIntentRequestDTO.java
@@ -0,0 +1,11 @@
+package com.andi.busapp.dto.PaymentIntent;
+
+import com.andi.busapp.dto.RequestDTO.PassengerRequest;
+
+import java.util.List;
+
+public record PaymentIntentRequestDTO(
+        Long tripId,
+        List<PassengerRequest> passengers
+)
+{}
---
+++ BackEnd-BusApp/BusApp/src/main/java/com/andi/busapp/dto/PaymentIntent/PaymentIntentResponse.java
@@ -0,0 +1,7 @@
+package com.andi.busapp.dto.PaymentIntent;
+
+public record PaymentIntentResponse(
+        String clientSecret,
+        String paymentIntentId
+)
+{}
